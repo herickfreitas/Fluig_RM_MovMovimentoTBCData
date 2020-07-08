f046433ddfc335e6fd7324200387540d4bc69a19
Herick Freitas <herickfreitas@gmail.com>
2020-07-08 18:38:21 -0300

    Implementando devolução da GEF

--- _Prj_TMOV_ReembAlim/forms/FormSolicitacaoReenbolso/events/displayFields.js
+++ _Prj_TMOV_ReembAlim/forms/FormSolicitacaoReenbolso/events/displayFields.js
@@ -2,11 +2,12 @@ function displayFields(form,customHTML){
 	
 	var activity = getValue('WKNumState');
 	var inicioPadrao = 0;
+	var ajustarSolicitacao = 180;
 	//var inicioProcesso = 2;
 	
 	log.info("displayFields WKNumState "+activity);
 	
-	if (activity != inicioPadrao)  {
+	if ((activity != inicioPadrao) && (activity != ajustarSolicitacao)) {
 		
 		var habilitar = false; // Informe True para Habilitar ou False para Desabilitar os campos
 	    var mapaForm = new java.util.HashMap();
